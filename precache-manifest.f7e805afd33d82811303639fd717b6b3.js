c2b9eeea43a4cbcc40d5be27edd5010270890f7b
Karolis <kkgrigas@gmail.com>
2020-04-15 17:45:05 +0300

    Push static site

---
+++ precache-manifest.f7e805afd33d82811303639fd717b6b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64e616e2de51c9acaaa027470cbefa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5982839ff8dab1f35f",
+    "url": "/static/css/main.c605ec05.chunk.css"
+  },
+  {
+    "revision": "eccd0075bf106801a469",
+    "url": "/static/js/2.84ac03b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.84ac03b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5982839ff8dab1f35f",
+    "url": "/static/js/main.d8902235.chunk.js"
+  },
+  {
+    "revision": "768a2063467787570328",
+    "url": "/static/js/runtime-main.ed555e06.js"
+  }
+]);
